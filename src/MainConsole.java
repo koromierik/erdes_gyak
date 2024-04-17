a96083957ea8b475c6d0cf1e4831b68c4cde126a
MatiasX <waldorfosmatyi@gmail.com>
2024-04-17 09:52:31 +0200

    kesz

---
+++ src/MainConsole.java
@@ -0,0 +1,12 @@
+import java.io.File;
+
+public class MainConsole {
+    
+    public MainConsole(String[] args) {
+        readFile(args[0]);
+    }
+    public void readFile(String fileName) {
+        File file = new File(fileName);
+        
+    }
+}
